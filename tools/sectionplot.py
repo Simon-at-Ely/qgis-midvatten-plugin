e8d5ad2a1e177d0df81313e53fcc66139a2103a8
Simon-at-Ely <simon.oconnor@btinternet.com>
2018-08-31 12:40:17 +0100

    Change to sectionplot.py to run new version of ui with Legend checkbox.
    Signed-off-by: Simon-at-Ely <simon.oconnor@btinternet.com>

--- tools/sectionplot.py
+++ tools/sectionplot.py
@@ -47,7 +47,7 @@ from PyQt4.QtCore import QCoreApplication
 
 #from ui.secplotdockwidget_ui import Ui_SecPlotDock
 from PyQt4 import uic
-Ui_SecPlotDock =  uic.loadUiType(os.path.join(os.path.dirname(__file__),'..','ui', 'plotdockwidget.ui'))[0]
+Ui_SecPlotDock =  uic.loadUiType(os.path.join(os.path.dirname(__file__),'..','ui', 'secplotdockwidget2_ui.ui'))[0]
 
 import definitions.midvatten_defs as defs
 from sampledem import qchain, sampling
