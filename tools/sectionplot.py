35a5cafa6ffed040adcbf3bc4117cc4c2abbebbf
Simon-at-Ely <simon.oconnor@btinternet.com>
2018-09-04 12:21:32 +0100

    Change to sectionplot.py, to fix hydrology plot
    Signed-off-by: Simon-at-Ely <simon.oconnor@btinternet.com>

--- tools/sectionplot.py
+++ tools/sectionplot.py
@@ -880,7 +880,7 @@ class SectionPlot(PyQt4.QtGui.QDockWidget, Ui_SecPlotDock):#the Ui_SecPlotDock  
         elif self.ms.settingsdict['secplottext'] == 'capacity':
             annotate_txt = self.capacity_txt
         elif self.ms.settingsdict['secplottext'] == 'hydroexplanation':
-            annotate_txt = self.hydroColors.get(self.capacity_txt, '')[0]
+            annotate_txt = self.hydroColors.get(self.capacity_txt, '')
         elif self.ms.settingsdict['secplottext'] == 'development':
             annotate_txt = self.development_txt
         else:
