437ef30d60c34787338d1554ad88581e1f2fb595
Simon-at-Ely <simon.oconnor@btinternet.com>
2018-09-10 10:17:47 +0100

    Change to sectionplot.py, to fix hydro explanation
    Signed-off-by: Simon-at-Ely <simon.oconnor@btinternet.com>

--- tools/sectionplot.py
+++ tools/sectionplot.py
@@ -713,6 +713,7 @@ class SectionPlot(PyQt4.QtGui.QDockWidget, Ui_SecPlotDock):#the Ui_SecPlotDock  
             return label + '_' + str(label_occurence + 1)
 
     def plot_geology(self):
+        print(str(self.ExistingPlotTypes))
         for Typ in self.ExistingPlotTypes:#Adding a plot for each "geoshort" that is identified
             #Try to get one setting per geoshort.
             _settings = copy.deepcopy(self.secplot_templates.loaded_template['geology_Axes_bar'])
