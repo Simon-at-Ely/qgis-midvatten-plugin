ed66a3e2f41c05a4024e70544a62e4ae360d5111
Simon-at-Ely <simon.oconnor@btinternet.com>
2018-09-26 13:35:16 +0100

    Change to sectionplot.py to debug zero width on single bar stratigraphy plot
    Signed-off-by: Simon-at-Ely <simon.oconnor@btinternet.com>

--- tools/sectionplot.py
+++ tools/sectionplot.py
@@ -197,7 +197,10 @@ class SectionPlot(PyQt4.QtGui.QDockWidget, Ui_SecPlotDock):#the Ui_SecPlotDock  
                 xmax, xmin =float(max(self.LengthAlong)), float(min(self.LengthAlong))
             else:
                 xmax, xmin = (float(max(self.LengthAlong))+0.1), float(min(self.LengthAlong))
+            print(str(xmin))
+            print(str(xmin))
             self.barwidth = (self.ms.settingsdict['secplotbw']/100.0)*(xmax -xmin)
+            print(str(self.barwidth))
 
             self.get_plot_data_2()
 
